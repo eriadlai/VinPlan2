bf3ca5065f6ca175ef072d32576c52a00c69650c
davidmb12 <46583555+davidmb12@users.noreply.github.com>
2020-11-04 02:34:21 -0600

    Added dto, repository, user service

---
+++ src/main/java/com/narval/Services/UserService.java
@@ -0,0 +1,14 @@
+package com.narval.Services;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+public class UserService {
+
+}
