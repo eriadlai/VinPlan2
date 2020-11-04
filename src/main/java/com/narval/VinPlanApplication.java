ec50055773ebf9d4da440663af30d015e43279e6
davidmb12 <46583555+davidmb12@users.noreply.github.com>
2020-11-04 00:58:15 -0600

    Init

---
+++ src/main/java/com/narval/VinPlanApplication.java
@@ -0,0 +1,13 @@
+package com.narval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinPlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/narval/VinPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
